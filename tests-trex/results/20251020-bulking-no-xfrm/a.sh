3b28b16871fed47554da75620799b1cb3f5f1627
Antony Antony <antony.antony@secunet.com>
2025-10-20 22:22:00 +0200

    plot new data set

--- tests-trex/results/20251020-bulking-no-xfrm/a.sh
+++ tests-trex/results/20251020-bulking-no-xfrm/a.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 : ${SUBDIR=results/20251017-bulking-no-xfrm}
 mkdir -p $SUBDIR
-./u1.py --pps 16M --flows-end 10 --frame-size 128 --duration 30 --csvfile $SUBDIR/both-bulk
-./u1.py --pps 25M --flows-start 11 --flows-end 15 --frame-size 128 --duration 30 --csvfile $SUBDIR/both-bulk-11
+./u1.py --pps 16M --flows-end 10 --frame-size 128 --duration 30 --csvfile $SUBDIR/both-bulk-1
+./u1.py --pps 25M --flows-start 11 --flows-end 15 --frame-size 128 --duration 30 --csvfile $SUBDIR/both-bulk-2
 ./u1.py --pps 16M --flows-end 15 --frame-size 128 --duration 30 --csvfile $SUBDIR/sender-bulk
 ./u1.py --pps 16M --flows-end 15 --frame-size 128 --duration 30 --csvfile $SUBDIR/receiver-bulk
 ./u1.py --pps 16M --flows-end 15 --frame-size 128 --duration 30 --csvfile $SUBDIR/no-bulk
