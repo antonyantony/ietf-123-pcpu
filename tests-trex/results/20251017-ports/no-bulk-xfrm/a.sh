ea40fd2ccbe0f001bbbb7fad5c48f9b555f98b0a
IETF 123 Hackethon <root@tatu.libreswan.org>
2025-10-20 12:15:30 +0000

    tests-trex/results/20251017-ports/no-bulk-xfrm

---
+++ tests-trex/results/20251017-ports/no-bulk-xfrm/a.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+: ${SUBDIR=results/20251017-ports/no-bulk-xfrm}
+mkdir -p $SUBDIR
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 1 --duration 30 --csvfile $SUBDIR/flows-1
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 2 --duration 30 --csvfile $SUBDIR/flows-2
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 4 --duration 30 --csvfile $SUBDIR/flows-4
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 8 --duration 30 --csvfile $SUBDIR/flows-8
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 16 --duration 30 --csvfile $SUBDIR/flows-16
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 32 --duration 30 --csvfile $SUBDIR/flows-32
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 64 --duration 30 --csvfile $SUBDIR/flows-64
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 128 --duration 30 --csvfile $SUBDIR/flows-128
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 256 --duration 30 --csvfile $SUBDIR/flows-256
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 512 --duration 30 --csvfile $SUBDIR/flows-512
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 1024 --duration 30 --csvfile $SUBDIR/flows-1024
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 2048 --duration 30 --csvfile $SUBDIR/flows-2048
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 4096 --duration 30 --csvfile $SUBDIR/flows-4096
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 8192 --duration 30 --csvfile $SUBDIR/flows-8192
+./u1.py --pps 0.8M --frame-size 128 --src-port 5000 --src-ports 16384 --duration 30 --csvfile $SUBDIR/flows-16384
