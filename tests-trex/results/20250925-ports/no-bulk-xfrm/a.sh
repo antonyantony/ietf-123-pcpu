befce2a01c0c4060f198257e7e6615f5a4f70978
Antony Antony <antony@phenome.org>
2026-02-11 13:39:33 +0100

    runer script

---
+++ tests-trex/results/20250925-ports/no-bulk-xfrm/a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SUBDIR=results/20250925-ports/no-bulk-xfrm
+mkdir -p $SUBDIR
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 1 --duration 30 --csvfile $SUBDIR/trex-flows-1-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 2 --duration 30 --csvfile $SUBDIR/20250925-ports/trex-flows-2-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 4 --duration 30 --csvfile $SUBDIR/trex-flows-4-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 8 --duration 30 --csvfile $SUBDIR/trex-flows-8-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 16 --duration 30 --csvfile $SUBDIR/trex-flows-16-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 128 --duration 30 --csvfile $SUBDIR/trex-flows-128-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 256 --duration 30 --csvfile $SUBDIR/trex-flows-256-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 512 --duration 30 --csvfile $SUBDIR/trex-flows-512-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 1024 --duration 30 --csvfile $SUBDIR/trex-flows-1014-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 4096 --duration 30 --csvfile $SUBDIR/trex-flows-4096-frame-size-128-nobulk-xfrm
+./u1.py --pps 0.7M --frame-size 128 --dst-ports 8192 --duration 30 --csvfile $SUBDIR/trex-flows-8192-frame-size-128-nobulk-xfrm
